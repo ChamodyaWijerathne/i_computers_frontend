93d2685a17b7c5e21801ad209c54aaa2ce1e5f38
Chamodya Wijerathne <chamodyawijerathne096@gmail.com>
2026-01-31 23:06:29 +0530

    Initialization🚀

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import ProductCard from "./components/productCard"
+import UserData from "./components/userData"
+import OnSaleNow from "./components/onSaleNow"
+
+function App() {
+
+  return (
+    <div className = "bg-red-300">
+      <UserData />
+      <OnSaleNow />
+
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/userData.jsx
@@ -0,0 +1,8 @@
+export default function UserData(){
+    return(
+        <div>
+            <img src="https://picsum.photos/id/237/200/300" className="rounded-2xl w-[33.5px] h-[33.5px]"/>
+            <h2>John Doe</h2>
+        </div>
+    )
+}
