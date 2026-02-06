81eaedc04796217246072fba08319a7a18c4ce5d
Chamodya Wijerathne <chamodyawijerathne096@gmail.com>
2026-02-06 21:41:45 +0530

    Add Test component and integrate into routing; update layout and functionality

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,21 +1,32 @@
+import { useState } from "react"
+
 export default function Test() {
+    let count = 0;
+
     return (
-        <div className="w-[600px] h-[600px] border-4 relative">
-            <div className="w-[500px] h-[500px] bg-amber-200 flex flex-row justify-center ">
-                <div className="w-[75px] h-[75px] bg-blue-600">
-                </div>
-                <div className="w-[75px] h-[75px] bg-green-600">
-                </div>
-                <div className="w-[75px] h-[75px] bg-red-600">
-                </div> 
-                <div className="w-[75px] h-[75px] bg-purple-600">
-                </div>
-                <div className="w-[75px] h-[75px] bg-pink-600">
-                </div>
-                <div className="w-[75px] h-[75px] bg-[#70cbe9]">
+        <div className="w-full h-full bg-yellow-300 flex justify-center items-center">
+            <div className="w-[400px] h-[400px] bg-white flex justify-center items-center flex-col">
+                <h1 className="text-[40px]">{count}</h1>
+                <div className="w-full h-[50px] flex justify-center items-center gap-2">
+                    <button onClick={
+                        ()=>{
+                            count = count - 1; 
+                        }
+                    } className="w-[100px] h-[45px] bg-red-700 text-white">
+                        Decrement
+                    </button>
+                    <button onClick={
+                        ()=>{
+                            console.log("Increment clicked");
+                        }
+                    }className="w-[100px] h-[45px] bg-green-700 text-white ml-5">
+                        Increment
+                    </button>
                 </div>
 
+
             </div>
+            
         </div>
     )
 }
