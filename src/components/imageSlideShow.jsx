fd38b295d9452adc333d3364c48bd10e9b7a5271
Chamodya Wijerathne <chamodyawijerathne096@gmail.com>
2026-02-22 17:35:30 +0530

    Day 23 - II

--- src/components/imageSlideShow.jsx
+++ src/components/imageSlideShow.jsx
@@ -4,10 +4,19 @@ export default function ImageSlideShow(props){
     const images = props.images
     const [activeImage, setActiveImage] = useState(0)
 
+    function getClasses(index){
+        if(index==activeImage){
+            return "w-[90px] h-[90px] object-contain rounded-[20px] border-3 border-accent/80 cursor-pointer"
+        }else{
+            return "w-[90px] h-[90px] object-contain rounded-[20px]  cursor-pointer opacity-100"
+        }
+    }
+    
+
     return(
         <div className="w-[500px] h-[600px] flex flex-col">
             <img src={images[activeImage]} alt="Product Image" className="w-full h-[500px] object-cover"/>
-            <div className="w-full h-[100px] flex flex-row px-4 gap-4 justify-center">
+            <div className="w-full h-[100px] flex flex-row px-4 gap-4 justify-center items-center">
                 {
                     images.map(
                         (img, index) =>{
@@ -17,7 +26,7 @@ export default function ImageSlideShow(props){
                                     setActiveImage(index)
                                 }
                             }
-                            key={index} src={img} className="w-[100px] h-[100px] object-contain rounded-[20px] border-4 border-accent cursor-pointer"/>
+                            key={index} src={img} className={getClasses(index)}/>
                         }
                     )
                 }
