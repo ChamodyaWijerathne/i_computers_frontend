57a2b07fc51c1ae8908c3e9e50acb7ce146faa6d
Chamodya Wijerathne <chamodyawijerathne096@gmail.com>
2026-04-16 03:54:48 +0530

    📱Implement product search functionality and update API call logic

--- src/pages/productPage.jsx
+++ src/pages/productPage.jsx
@@ -7,11 +7,16 @@ import LoadingAnimation from "../components/loadingAnimation";
 export default function ProductPage(){
     const [products, setProducts] = useState([])
     const [loading, setLoading] = useState(true)
+    const [searchQuery, setSearchQuery] = useState("")
 
     useEffect(
         ()=>{
             if(loading){
-                axios.get(import.meta.env.VITE_API_URL + "/products")
+                let url = import.meta.env.VITE_API_URL + "/products/"
+                if(searchQuery !=""){
+                    url = import.meta.env.VITE_API_URL + "/products/search/"+searchQuery
+                }
+                axios.get(url)
                 .then(
                     (response)=>{
                         setProducts(response.data)
@@ -24,13 +29,37 @@ export default function ProductPage(){
                     }
                 )
             }
-        },[loading]
+        },[loading,]
     )
 
     return(
-        <div className="flex justify-center items-center flex-wrap bg-primary">
+        <div className="flex justify-center items-center flex-wrap bg-primary relative pt-[80px]">
             {
                 loading && <LoadingAnimation/>
+            }
+            {
+                <div className="w-full h-[60px] fixed top-[100px] z-99 flex justify-center items-center backdrop-blur-lg gap-5">
+                    <input 
+                        type="text" 
+                        placeholder="Search for products..." 
+                        className="w-[50%] h-[40px] rounded-md px-4 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-sm text-gray-700 placeholder-gray-400 transition duration-300 ease-in-out focus:shadow-lg focus:shadow-blue-500/50 focus:scale-105 "
+                        value={searchQuery}
+                        onChange={(e) => {
+                            setSearchQuery(e.target.value)
+                            setLoading(true)
+                            
+                        }}
+                    
+                    />
+                    <button 
+                        className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition duration-300 ease-in-out focus:shadow-lg focus:shadow-blue-500/50 focus:scale-105"
+                        onClick={()=>{
+                            setSearchQuery("")
+                            setLoading(true)
+                        }}
+                    > Get All Products
+                    </button>
+                </div>
             }
             {
                products.map(
